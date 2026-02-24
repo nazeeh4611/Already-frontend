736636e0a8401c3d77353ecff992611470904d58
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-02-24 19:32:09 +0400

    after domain

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,5 +1,5 @@
-export const baseurl='https://wavesgobal-backend.onrender.com/api/'
-export const ClientId = "418114033455-b5m2g67ikop5a7nte7jccg5rsasah0av.apps.googleusercontent.com"
+export const baseurl='https://already-backend-production.up.railway.app/api/'
+export const ClientId = "418114033455-j866998n0dckm9u8ptt0i32ps766t8l2.apps.googleusercontent.com"
 
 // export const baseurl = `http://localhost:3000/api/`
 
