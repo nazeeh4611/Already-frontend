9ba9de0af262763b75c7025534163a139aa496b9
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-02-28 01:01:03 +0400

    pass

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,7 +1,7 @@
-// export const baseurl='https://already-backend-production.up.railway.app/api/'
+export const baseurl='https://already-backend-production.up.railway.app/api/'
 export const ClientId = "418114033455-j866998n0dckm9u8ptt0i32ps766t8l2.apps.googleusercontent.com"
 
-export const baseurl = `http://localhost:3000/api/`
+// export const baseurl = `http://localhost:3000/api/`
 
 
 
