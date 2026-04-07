554b1471183c9a0216782f4369d78e64e846801f
Nazeeh Nahaban <nazeehnahaban09@gmail.com>
2026-04-07 22:26:20 +0400

    render

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -1,4 +1,5 @@
-export const baseurl='https://already-backend-production.up.railway.app/api/'
+// export const baseurl='https://already-backend-production.up.railway.app/api/'
+export const baseurl='https://alraqy-i226.onrender.com/api/'
 export const ClientId = "418114033455-j866998n0dckm9u8ptt0i32ps766t8l2.apps.googleusercontent.com"
 
 // export const baseurl = `http://localhost:3000/api/`
